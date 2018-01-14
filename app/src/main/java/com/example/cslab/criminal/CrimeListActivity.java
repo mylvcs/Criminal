3e3025004d84d55ac26478be2ce839150d52f7ca
mylvcs <wmy0828@nuaa.edu.cn>
2018-01-14 16:59:27 +0800

    Page 134

--- app/src/main/java/com/example/cslab/criminal/CrimeListActivity.java
+++ app/src/main/java/com/example/cslab/criminal/CrimeListActivity.java
@@ -8,7 +8,7 @@ import android.support.v4.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
     protected Fragment createFragment(){
-        return  new CrimeListActivity();
+        return  new CrimeListFragment();
 
     }
 }
--- app/src/main/java/com/example/cslab/criminal/CrimeListFragment.java
+++ app/src/main/java/com/example/cslab/criminal/CrimeListFragment.java
@@ -1,10 +1,34 @@
 package com.example.cslab.criminal;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by CSLab on 2018/1/14.
  */
 
 public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+            public class CrimeHolder extends RecyclerView.ViewHolder {
+                public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+                    super(inflater.inflate(R.layout.list_item_crime, parent, false));
+                }
+            }
+
 }
