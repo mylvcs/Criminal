96306c8ae1107fe125d99de0678dac483be30dd0
mylvcs <wmy0828@nuaa.edu.cn>
2018-01-14 09:08:47 +0800

    Page132

---
+++ app/src/main/java/com/example/cslab/criminal/CrimeLab.java
@@ -0,0 +1,46 @@
+package com.example.cslab.criminal;
+
+import android.content.Context;
+import android.text.method.NumberKeyListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by CSLab on 2018/1/14.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context){
+        if (sCrimeLab == null){
+    sCrimeLab = new CrimeLab(context);
+        }return sCrimeLab;
+    }
+
+    public List<Crime> getmCrimes() {
+        return mCrimes;
+    }
+
+    private CrimeLab (Context context){
+        mCrimes= new ArrayList<>();
+        for (int i=0; i<100; i++){
+            Crime crime = new Crime();
+            crime.setmTitle("Crime #"+i);
+            crime.setmSolved(i%2==0);
+            mCrimes.add(crime);
+        }
+    }
+    private Crime getCrime(UUID id){
+        for(Crime crime: mCrimes) {
+            if (crime.getMid().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
